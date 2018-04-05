f8b4c1b27328361a75b8cb3dea9900d4aba47e52
Your Name <wherearemyrepositories@github.com>
2018-04-05 12:44:35 +0200

    new lab

---
+++ com_init.m
@@ -0,0 +1,4 @@
+s = serial('/dev/ttyUSB1');
+s.BaudRate = 57600;
+fopen(s);
+
---
+++ control.m
@@ -0,0 +1,17 @@
+%fprintf(s,'x200 y200 z200\n');
+t = 0:0.1:2*pi;
+
+
+traj1 = round(200*sin(t));
+traj2 = round(200*sin(t+2*pi/3));
+traj3 = round(200*sin(t+4*pi/3));
+
+kroki1 = traj1(2:end) - traj1(1:end-1);
+kroki2 = traj2(2:end) - traj2(1:end-1);
+
+kroki3 = traj3(2:end) - traj3(1:end-1);
+
+for  i=1:size(kroki1,2) 
+    fprintf(s,'x%d y%d z%d\n',[kroki1(i) kroki2(i) kroki3(i)]);
+    pause(0.1)
+end
